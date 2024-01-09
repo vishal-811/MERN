acd10ac0e2a414c264adcb94ff47dcc7deabb796
vishalsharma <vishalssharma811@gmail.com>
2024-01-09 23:47:30 +0530

    working on authentication

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://user2000:@cluster0.pdnkicq.mongodb.net/courseDb');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
-});
+    username:{
+       type: string,
+        required:[true, 'username is  required']
+    },
+    password:{
+        type:string,
+        required:[true, 'password is required'],
+        length:6
+    },
+ },
+
+// {
+    // timestamps:true,
+// }
+
+);
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,50 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const {Admin, Course} =require('../db')
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username =req.body.username;
+    const password=req.body.password;
+
+    const newAdmin =await Admin.create({
+        username:username,
+        password:password
+    })
+    res.status(200).json({msg:"Admin created Successfully"});
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    //  const title =req.body.title;
+    //  const description =req.body.description;
+    //  const price =req.body.price;
+    //  const imageLink =req.body.imageLink;
+
+    const {title ,description, price,imageLink} =req.body;
+    const newcourse =await Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+    res.status(200).json({msg:"course created successfully"})
+    // id wala logic pending hn
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+       try {
+        const courses=await Course.find({})
+        res.status(200).json(courses)
+       } catch (error) {
+          res.status(400).json({msg:"something went wrong"})
+       }       
 });
 
 module.exports = router;
+
+// Input: Headers: { 'username': 'username', 'password': 'password' }
+//   Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
